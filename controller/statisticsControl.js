45c2458a6bdd93dbe25a521e4d0aa4e4c4e8f9f3
Tigris <Tigris_wzd@163.com>
2017-11-13 11:25:43 +0800

    commit 11/13

--- controller/statisticsControl.js
+++ controller/statisticsControl.js
@@ -2,12 +2,47 @@ var statisticsControl = {}
 var statisticsDao = require('../dao/statisticsDao')
 var userLang = require('../lang/userLang')
 
-statisticsControl.getTotalExpend=function(app){
+statisticsControl.getTotalExpend=function(app){ // 统计总共支出
   app.get('/get-totalExpend',function(req,res){
   	statisticsDao.getTotalExpend(req.query).then(function(data){
       res.status(200),
       res.json(data)
   	})
+  })
+}
+
+statisticsControl.getechartsData=function(app){
+  app.get('/get-echartsData',function(req,res){
+  	// income
+  	let income1 = statisticsDao.getJanIncome(req.query)
+  	let income2 = statisticsDao.getFebIncome(req.query)
+  	let income3 = statisticsDao.getMarIncome(req.query)
+  	let income4 = statisticsDao.getAprIncome(req.query)
+  	let income5 = statisticsDao.getMayIncome(req.query)
+  	let income6 = statisticsDao.getJuneIncome(req.query)
+  	let income7 = statisticsDao.getJulyIncome(req.query)
+  	let income8 = statisticsDao.getAugIncome(req.query)
+  	let income9 = statisticsDao.getSeptIncome(req.query)
+  	let income10 = statisticsDao.getOctIncome(req.query)
+  	let income11 = statisticsDao.getNovIncome(req.query)
+  	let income12 = statisticsDao.getDecIncome(req.query)
+  	// expend
+  	let expend1 = statisticsDao.getJanExpend(req.query)
+  	let expend2 = statisticsDao.getFebExpend(req.query)
+  	let expend3 = statisticsDao.getMarExpend(req.query)
+  	let expend4 = statisticsDao.getAprExpend(req.query)
+  	let expend5 = statisticsDao.getMayExpend(req.query)
+  	let expend6 = statisticsDao.getJuneExpend(req.query)
+  	let expend7 = statisticsDao.getJulyExpend(req.query)
+  	let expend8 = statisticsDao.getAugExpend(req.query)
+  	let expend9 = statisticsDao.getSeptExpend(req.query)
+  	let expend10 = statisticsDao.getOctExpend(req.query)
+  	let expend11 = statisticsDao.getNovExpend(req.query)
+  	let expend12 = statisticsDao.getDecExpend(req.query)
+  	Promise.all([income1, income2, income3, income4, income5, income6, income7, income8, income9, income10, income11, income12, expend1, expend2, expend3, expend4, expend5, expend6, expend7, expend8, expend9, expend10, expend11, expend12]).then(values => { 
+  	  res.status(200),
+      res.json(values)
+	  })
   })
 }
 
--- dao/statisticsDao.js
+++ dao/statisticsDao.js
@@ -4,7 +4,7 @@ var statisticsDao = {}
 
 statisticsDao.getTotalExpend=function(data){
 	return new Promise(function(resolve,reject){
-	    var sql = 'SELECT baccount,bcategory,bincomeorpay FROM account_book'
+	    var sql = 'SELECT baccount,bcategory,bincomeorpay,bdate FROM account_book WHERE uid = ' + data.uid
 	    // console.log(sql)
 	    jdbc.query(sql, function (error, results, fields) {
 		    if (error) {
@@ -14,6 +14,345 @@ statisticsDao.getTotalExpend=function(data){
 		    }else{
 		      resolve(results)
 		    }
+		})
+	})
+}
+
+statisticsDao.getJanIncome=function(data){ // 1月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-01-01" and bdate < "2017-02-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getFebIncome=function(data){ // 2月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-02-01" and bdate < "2017-03-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getMarIncome=function(data){ // 三月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-03-01" and bdate < "2017-04-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getAprIncome=function(data){ // 四月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-04-01" and bdate < "2017-05-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getMayIncome=function(data){ // 五月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-05-01" and bdate < "2017-06-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getJuneIncome=function(data){ // 六月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-06-01" and bdate < "2017-07-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getJulyIncome=function(data){ // 七月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-07-01" and bdate < "2017-08-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getAugIncome=function(data){ // 八月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-08-01" and bdate < "2017-09-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getSeptIncome=function(data){ // 九月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-09-01" and bdate < "2017-10-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getOctIncome=function(data){ // 十月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-10-01" and bdate < "2017-11-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getNovIncome=function(data){ // 十一月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-11-01" and bdate < "2017-12-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getDecIncome=function(data){ // 十二月收入
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 1 and uid = '+ data.uid +' and bdate >= "2017-12-01" and bdate < "2018-01-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+
+statisticsDao.getJanExpend=function(data){ // 1月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-01-01" and bdate < "2017-02-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+
+statisticsDao.getFebExpend=function(data){ // 2月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-02-01" and bdate < "2017-03-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getMarExpend=function(data){ // 3月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-03-01" and bdate < "2017-04-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getAprExpend=function(data){ // 4月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-04-01" and bdate < "2017-05-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getMayExpend=function(data){ // 5月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-05-01" and bdate < "2017-06-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getJuneExpend=function(data){ // 6月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-06-01" and bdate < "2017-07-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getJulyExpend=function(data){ // 7月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-07-01" and bdate < "2017-08-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getAugExpend=function(data){ // 8月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-08-01" and bdate < "2017-09-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getSeptExpend=function(data){ // 9月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-09-01" and bdate < "2017-10-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getOctExpend=function(data){ // 10月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-10-01" and bdate < "2017-11-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getNovExpend=function(data){ // 11月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-11-01" and bdate < "2017-12-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
+		})
+	})
+}
+statisticsDao.getDecExpend=function(data){ // 12月支出
+	return new Promise(function(resolve, reject) {
+		var sql = 'SELECT sum(baccount) account from account_book WHERE bincomeorpay = 0 and uid = '+ data.uid +' and bdate >= "2017-12-01" and bdate < "2018-01-01"'
+		jdbc.query(sql, function(error, results, fields) {
+			if (error) {
+				return jdbc.rollback(function() {
+			        throw error
+			    });
+			} else {
+				resolve(results)
+			}
 		})
 	})
 }
